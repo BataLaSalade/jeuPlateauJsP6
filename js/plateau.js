c8fbe45bc97620a9ca56996eb4c84320f1ebb9d9
BataLaSalade <dsfmarina@gmail.com>
2018-07-27 16:05:38 +0200

    refacto : position prototype --> add 3 methods to manage object's position / refacto : map object --> add number attributes to set objects creation - add display method with object and number for parameters

--- js/plateau.js
+++ js/plateau.js
@@ -5,53 +5,59 @@ var $row = $("<div />", {
 var $square = $("<div />", {
     class: 'square'
 });
-var map = {
-    rows : 10,
-    columns : 10,
-    genMap : function (){
-        for (var i = 0; i < this.rows; i++) {
-            var tmpRow = $row.clone();
-            for (var j = 0; j < this.columns; j++) {
-                tmpRow.append($square.clone());
-            }
-            $("#wrapper").append(tmpRow);
-        }
-    }
-};
-
-function getRandomPosition (min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+var $obstacle = $("<div />", {
+    class : "obstacle"
+});
 
 var Position = {
-    initPosition : function(colIndex, rowIndex){
+    setPosition: function(colIndex, rowIndex){
         this.colIndex = colIndex;
         this.rowIndex = rowIndex;
+    },
+    initRandomPosition: function(colMaxIndex, rowMaxIndex){
+        this.colIndex = this.genRandomPosition(0, colMaxIndex);
+        this.rowIndex = this.genRandomPosition(0, rowMaxIndex);
+    },
+    genRandomPosition: function (min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 }
 
 var obstaclePosition = Object.create(Position);
-randomCol = getRandomPosition(0, map.columns-1)
-randomRow = getRandomPosition(0, map.rows-1)
-obstaclePosition.initPosition(randomCol, randomRow);
-
-
-
-var $obstable = $("<div />", {
-    class : "obstable"
-    
-});
-
-var obstable = {
-    nbObstables : 25,
+var obstacle = {
     position: obstaclePosition,
-   
+    // visuel
 };
-console.log(obstable)
+console.log(obstacle)
+
+
+var map = {
+    rows : 10,
+    columns : 10,
+    nbObstacles : 25,
+    nbPlayers : 2,
+    nbWeapons : 4,
+    display : function (objet, number) {
+        for (var j = 0; j < number; j++) {
+            this.object = objet.position.initRandomPosition(map.columns-1, map.rows-1);
+            $(".line:eq("+ objet.position.rowIndex +") .square:eq("+ objet.position.colIndex +")").append($obstacle.clone());
+        };
+    },
+    genMap : function (){
+        for (var i = 0; i < this.rows; i++) {
+            var tmpRow = $row.clone();
+            for (var k = 0; k < this.columns; k++) {
+                tmpRow.append($square.clone());
+            };
+            $("#wrapper").append(tmpRow);
+        };
+    }
+};
 
 $(function () {
     map.genMap();
-
+    map.display(obstacle, map.nbObstacles);
+    
 });
