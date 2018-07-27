cd871a58a9bd86c30733013c3ae5b5bd9c5ed8f5
BataLaSalade <dsfmarina@gmail.com>
2018-07-27 18:03:22 +0200

    refacto

--- js/plateau.js
+++ js/plateau.js
@@ -8,13 +8,21 @@ var $square = $("<div />", {
 var $obstacle = $("<div />", {
     class : "obstacle"
 });
+var $player = $("<div />", {
+    class : "player"
+});
+var $weapon = $("<div />", {
+    class : "weapon"
+});
 
 var Position = {
+    colIndex: 0,
+    rowIndex: 0,
     setPosition: function(colIndex, rowIndex){
         this.colIndex = colIndex;
         this.rowIndex = rowIndex;
     },
-    initRandomPosition: function(colMaxIndex, rowMaxIndex){
+    initRandomPosition: function(colMaxIndex, rowMaxIndex) {
         this.colIndex = this.genRandomPosition(0, colMaxIndex);
         this.rowIndex = this.genRandomPosition(0, rowMaxIndex);
     },
@@ -25,13 +33,19 @@ var Position = {
     }
 }
 
-var obstaclePosition = Object.create(Position);
-var obstacle = {
-    position: obstaclePosition,
-    // visuel
+var Obstacle = {
+    init: function (position){
+        this.position = position
+    }
 };
-console.log(obstacle)
-
+/*var Player = {
+    init : function (nom, position){
+        this.nom = nom,
+        this.position = position,
+        this.sante = 100
+    }
+    //visuel
+};*/
 
 var map = {
     rows : 10,
@@ -39,25 +53,49 @@ var map = {
     nbObstacles : 25,
     nbPlayers : 2,
     nbWeapons : 4,
-    display : function (objet, number) {
-        for (var j = 0; j < number; j++) {
-            this.object = objet.position.initRandomPosition(map.columns-1, map.rows-1);
-            $(".line:eq("+ objet.position.rowIndex +") .square:eq("+ objet.position.colIndex +")").append($obstacle.clone());
-        };
+    display : function (objets, container) {
+        for (var j = 0; j < objets.length; j++) {
+            $(".line:eq("+ objets[j].position.rowIndex +") .square:eq("+ objets[j].position.colIndex +")").append(container.clone());
+        }
     },
     genMap : function (){
         for (var i = 0; i < this.rows; i++) {
             var tmpRow = $row.clone();
             for (var k = 0; k < this.columns; k++) {
                 tmpRow.append($square.clone());
-            };
+            }
             $("#wrapper").append(tmpRow);
-        };
+        }
     }
 };
 
+function genListObstacle(nbObstacles, colMaxIndex, rowMaxIndex) {
+    var listObstacle = [];
+    for (var j = 0; j < nbObstacles; j++) {
+        var tmpPosition = Object.create(Position)     
+        var currentObstacle = Object.create(Obstacle)   
+        tmpPosition.initRandomPosition(colMaxIndex, rowMaxIndex);
+        currentObstacle.init(tmpPosition)
+        listObstacle.push(currentObstacle)
+    }
+    return listObstacle;
+}
+/*function genListPlayer(nbPlayers, colMaxIndex, rowMaxIndex) {
+    var listPlayer = [];
+    for (var j = 0; j < nbPlayers; j++) {
+        var tmpPosition = Object.create(Position)     
+        var currentPlayer = Object.create(Player)
+        tmpPosition.initRandomPosition(colMaxIndex, rowMaxIndex);
+        currentPlayer.init(tmpPosition)
+        listPlayer.push(currentPlayer)
+    }
+    return listPlayer;
+}*/
+
 $(function () {
     map.genMap();
-    map.display(obstacle, map.nbObstacles);
-    
+    var obstacles = genListObstacle(map.nbObstacles, map.columns-1, map.rows-1);
+    map.display(obstacles, $obstacle);
+   // var players = genListPlayer(map.nbPlayers, map.columns-1, map.rows-1);
+   // map.display(players, $player)
 });
