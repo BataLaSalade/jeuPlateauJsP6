2260bd2035c9c456d350dea559cd594a6249741e
BataLaSalade <dsfmarina@gmail.com>
2018-08-27 14:34:59 +0200

    change on genlistWeapon method

--- js/weapons.js
+++ js/weapons.js
@@ -14,15 +14,21 @@ var Weapon = {
 var weaponNames = ["Epée en bois","Baton", "Arc", "Epée", "Lance"];
 var weaponListUrl = ["./img/png/weapon_epeeBois.png","./img/png/weapon_baton.png", "./img/png/weapon_arc.png", "./img/png/weapon_epee.png", "./img/png/weapon_lance.png"]
 
-function genListWeapon(nbWeapons, colMaxIndex, rowMaxIndex) {
+
+
+function genListWeapon (nbWeapons) {
     var listWeapons = [];
     var damage = 10;
-    for (var j = 1; j < nbWeapons; j++) {
-        var tmpPosition = getCheckedPosition(colMaxIndex, rowMaxIndex);
+    for (var i = 1; i < nbWeapons; i++) {
+        var randomIndex = getRandomIndexPosition(0, listAllPositions.length);
+        var randomAvailablePosition = listAllPositions[randomIndex];
+        var tmpPosition = Object.create(Position);
+        tmpPosition.setPosition(randomAvailablePosition.colIndex, randomAvailablePosition.rowIndex);
         var currentWeapon = Object.create(Weapon);
-        damage += 5;
-        currentWeapon.init(tmpPosition, weaponNames[j], damage, weaponListUrl[j]);
+        damage += 5
+        currentWeapon.init(tmpPosition, weaponNames[i], damage, weaponListUrl[i]);
         listWeapons.push(currentWeapon);
+        listAllPositions.splice(randomIndex,1);
     }
     return listWeapons;
- }
+}
