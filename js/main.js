ce307e23510d3f7e2d5783ca6246675bc5a55335
BataLaSalade <dsfmarina@gmail.com>
2018-10-05 15:52:06 +0200

    in main.js remplace all listObject[1] that refer to player by mapContainer.players

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var listObjects = [];
+
 var mapContainer = {};
 
 $(function ($) {
@@ -7,37 +7,34 @@ $(function ($) {
     console.log(mapCellPositions.length);
 
     mapContainer.obstacles = genListObstacle(map.nbObstacles);
-    listObjects.push(mapContainer.obstacles);
     console.log(mapContainer.obstacles)
     map.display(mapContainer.obstacles, $obstacle, "obstacle");
     console.log(mapCellPositions.length);
 
     mapContainer.players = genListPlayer(map.nbPlayers);
-    listObjects.push(mapContainer.players);
     console.log(mapContainer.players)
     map.display(mapContainer.players, $player, "player");
     console.log(mapCellPositions.length);
-    listObjects[1][0].createPositionToMove(listObjects[1][0].position);
-    listObjects[1][1].createPositionToMove(listObjects[1][1].position);
+    mapContainer.players[0].createPositionToMove(mapContainer.players[0].position);
+    mapContainer.players[1].createPositionToMove(mapContainer.players[1].position);
     
     mapContainer.weapons = genListWeapon(map.nbWeapons);
-    listObjects.push(mapContainer.weapons);
     console.log(mapContainer.weapons);
     map.display(mapContainer.weapons,$weapon, "weapon");
     console.log(mapCellPositions.length);
 
     $("#wrapper").on("click", ".Enable", function(e) {
-        remove(listObjects[1][0]);
+        remove(mapContainer.players[0]);
         console.log("click", e.target);
         var colIndexNextMove = Number($(e.target).attr("colindex"));
         var rowIndexNextMove = Number($(e.target).attr("rowindex"));
-        listObjects[1][0].position.setPosition(colIndexNextMove, rowIndexNextMove);
-        listObjects[1][0].createPositionToMove(listObjects[1][0].position);
-        moveToCell(listObjects[1][0], $player, "player",0);
+        mapContainer.players[0].position.setPosition(colIndexNextMove, rowIndexNextMove);
+        mapContainer.players[0].createPositionToMove(mapContainer.players[0].position);
+        moveToCell(mapContainer.players[0], $player, "player",0);
         showAllWeapon();
         if ($(e.target).hasClass("weapon")) {
             console.log("une arme !!");
-            managePlayerWeapon(listObjects[1][0]);
+            managePlayerWeapon(mapContainer.players[0]);
         }
         
     });
