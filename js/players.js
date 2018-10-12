2fe93bc50372707d19c98cb4f3d7b9c7c9367298
BataLaSalade <dsfmarina@gmail.com>
2018-10-12 16:41:35 +0200

    refato createPositionToMove Done

--- js/players.js
+++ js/players.js
@@ -21,7 +21,7 @@ var Player = {
     },
 
     createPositionToMove : function () {
-        var moveMaxNumber = 1;
+        var moveMaxNumber = 3;
         var currentPosition = this.position;
         var directions = ["R","L","T","B"];
         var scope = this;
@@ -34,23 +34,14 @@ var Player = {
                 var positionOnTheMap = positionToMove.isOnTheMap();
                 if (positionOnTheMap) {
                     scope.listOfPositionToMove.push(positionToMove);
-                    var border = "#e8d952 1px solid";
-                    var cellAccess = "Enable";
-                    var isDisable = false;
-                    mapContainer.obstacles.forEach(function(obstacle){
-                        isDisable = doesAtLeatOneObjectOnPosition (positionToMove, obstacle, isDisable)
-                    });
-                    displayDisableCell (positionToMove, isDisable, hasNoObjectOnTheWay, border, cellAccess);
-                    mapContainer.players.forEach(function(player){
-                       isDisable = doesAtLeatOneObjectOnPosition (positionToMove, player, isDisable);        
-                    });
-                    displayDisableCell (positionToMove, isDisable, hasNoObjectOnTheWay, border, cellAccess);
-                    //var cell = $(".line:eq("+ (positionToMove.rowIndex) +") .square:eq("+ (positionToMove.colIndex) +")")
-                    //cell.css("border", border).addClass(cellAccess);
+                    var stopDisplay = displayAvailableCellAroundPlayer (mapContainer.obstacles, mapContainer.players, positionToMove, hasNoObjectOnTheWay);
                 };
+                if (stopDisplay) {
+                    hasNoObjectOnTheWay = false;
+                }
                 i++;
             }
-        })
+        });
     },
 
     playerWearWeapon : function () {
@@ -123,3 +114,20 @@ function displayDisableCell (positionToMove, cellStatus, hasNoObjectOnTheWay, bo
     $(".line:eq("+ (positionToMove.rowIndex) +") .square:eq("+ (positionToMove.colIndex) +")").css("border", border).addClass(cellAccess);
 }
 
+function displayAvailableCellAroundPlayer (listObstacles, listPlayer, positionToMove, hasNoObjectOnTheWay) {
+    var border = "#e8d952 1px solid";
+    var cellAccess = "Enable";
+    var isDisable = false;
+    var positionToMove = positionToMove;
+    var hasNoObjectOnTheWay = hasNoObjectOnTheWay;
+    listObstacles.forEach(function(obstacle){
+        isDisable = doesAtLeatOneObjectOnPosition (positionToMove, obstacle, isDisable)
+    });
+    displayDisableCell (positionToMove, isDisable, hasNoObjectOnTheWay, border, cellAccess);
+    listPlayer.forEach(function(player){
+        isDisable = doesAtLeatOneObjectOnPosition (positionToMove, player, isDisable)
+    });
+    displayDisableCell (positionToMove, isDisable, hasNoObjectOnTheWay, border, cellAccess);
+
+    return isDisable;
+}
